70b5086244f5e6059ec64301dfb8e1de82bf8d91
Jason <jimanproductions@gmail.com>
2022-05-01 11:33:02 +0200

    new commit, been working on wrong file

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-// selects items of the DOM
-let add = document.getElementById("addItem")
+let form = document.querySelectorAll(".task");
+let testli = document.querySelector("#testLi");
+let testp = document.querySelector("#testp");
 
-//adding function for adding item to list
-let addTask = function() {
-  console.log("Added Item!");
+//Create a array of all items currently in list.
+let currentList = ["Walk dog", "Sleep"];
+
+/*Function for getting data from Form*/
+let getData = () => {
+  let dataValue = newTask.value;
+  console.log("inpuuted data: " + dataValue);
 }
 
-let test = function(){
-    console.log("test!");
+testli.innerHTML = currentList[1];
+function test() {
+testp.innerHTML = "lol this is a test!";
 }
 
+test();
 
-//uses addEventListenr from General implementation considerations
-add.addEventListener("click", addTask);
