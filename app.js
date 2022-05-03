a0abe7311368808d59cf6fd0fe9fb629d1b5f126
Jason <jimanproductions@gmail.com>
2022-05-03 17:53:33 +0200

    delete button provides message back to user

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 /*TODO: 
   [x]How to use foreach
   []Add new elements to html using javascript
-    []Add Buttons
+    [x]Create Buttons Elements
+      [x]Edit button
+      [x]Delete button
     [x]Add new text
     []add button functionality
+      []Delete
+      []Edit
   []Array functionalities
     []Add
     []Delete
@@ -23,10 +27,10 @@
 let form = document.querySelector("#addItem");
 let data = document.querySelector("#task");
 let testli = document.querySelector("#testli"); 
-
+let removes = document.querySelectorAll(".delete");
 
 let tasks = ["Walk Cat", "Walk Dog", "Walk Emu", "Talk to Emu", "Drink some beers", "Lift some weights or something"]
-
+let tasksFinished = [];
 
 /* Adds task to array function */
 let addTask = () =>{
@@ -40,30 +44,32 @@ let populateList = (t) =>{
   const newLi = document.createElement("li");
   const newLiContent = document.createTextNode(t);
   const checkBox = document.createElement("input");
-
-
-  newLi.appendChild(newLiContent);
-
   //creating new button
   const button = document.createElement("button");
+  const deleteButton = document.createElement("button");
 
+  //sets element attributes
   checkBox.type = "checkBox";
-
   button.innerText = "Editbra!";
   button.className = "edit";
+  deleteButton.innerText = "Delete";
+  deleteButton.className = "delete";
 
-  newLi.appendChild(button);
+  //append or add items to preexisting element
   newLi.appendChild(checkBox);
+  newLi.appendChild(newLiContent);
+  newLi.appendChild(button);
+  newLi.appendChild(deleteButton);
   
+  //selects element you are adding to
   const currentDiv = document.getElementById("ulthingie");
   //adds element
   currentDiv.append(newLi);
 }
 
-// let items = ["item1", "item2", "item3"]
-
-
+//populates list with array
 tasks.forEach(populateList);
+
 
 
 /*Need to create a new element using createElement */
@@ -79,6 +85,11 @@ let addElement = () => {
 }
 
 
+/*Delete item */
+let deleteItem = () => {
+  console.log("You clicked on the delete button!");
+}
+
 
 
 
@@ -92,6 +103,12 @@ let getData = () => {
 
 /* Calls on eventListener for click event */
 form.addEventListener("click", addTask)
+
+/*Needs to put the remove in a loop as there are multiple objects that are not loaded based on the DOM or something like that.*/
+for (const remove of removes){
+remove.addEventListener("click", deleteItem)
+}
+
 
 testli.innerHTML = "ASDFASDAFSDFAD";
 
