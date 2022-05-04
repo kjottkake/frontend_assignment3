695db3267235fd91d6bb8d7484e72baf2a1b6152
Jason <jimanproductions@gmail.com>
2022-05-04 14:22:43 +0200

    clears field after submission

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
       []Delete
       []Edit
   []Array functionalities
-    []Add
+    [x]Add
     []Delete
     []Edit
   []Delete Items
@@ -17,10 +17,9 @@
       []Confirm
       []Cancel
   []Add Item
+    [x]Adds item
+    []Add on enter
 */
-
-
-
 
 
 /*Selects various dom elements*/
@@ -34,7 +33,8 @@ let tasksFinished = [];
 
 /* Adds task to array function */
 let addTask = () => {
-  let newTask = getData();
+
+  let newTask = data.value;
   tasks.push(newTask);
   console.log(tasks);
 
@@ -63,39 +63,40 @@ let addTask = () => {
   //adds element
   currentDiv.append(newLi);
   // populateList(tasks);
+  data.value = '';
 }
 
 
 //Creates elements from array
-let populateList = (t) => {
-  const newLi = document.createElement("li");
-  const newLiContent = document.createTextNode(t);
-  const checkBox = document.createElement("input");
-  //creating new button
-  const button = document.createElement("button");
-  const deleteButton = document.createElement("button");
+// let populateList = (t) => {
+//   const newLi = document.createElement("li");
+//   const newLiContent = document.createTextNode(t);
+//   const checkBox = document.createElement("input");
+//   //creating new button
+//   const button = document.createElement("button");
+//   const deleteButton = document.createElement("button");
 
-  //sets element attributes
-  checkBox.type = "checkBox";
-  button.innerText = "Editbra!";
-  button.className = "edit";
-  deleteButton.innerText = "Delete";
-  deleteButton.className = "delete";
+//   //sets element attributes
+//   checkBox.type = "checkBox";
+//   button.innerText = "Editbra!";
+//   button.className = "edit";
+//   deleteButton.innerText = "Delete";
+//   deleteButton.className = "delete";
 
-  //append or add items to preexisting element
-  newLi.appendChild(checkBox);
-  newLi.appendChild(newLiContent);
-  newLi.appendChild(button);
-  newLi.appendChild(deleteButton);
+//   //append or add items to preexisting element
+//   newLi.appendChild(checkBox);
+//   newLi.appendChild(newLiContent);
+//   newLi.appendChild(button);
+//   newLi.appendChild(deleteButton);
 
-  //selects element you are adding to
-  const currentDiv = document.getElementById("ulthingie");
-  //adds element
-  currentDiv.append(newLi);
-}
+//   //selects element you are adding to
+//   const currentDiv = document.getElementById("ulthingie");
+//   //adds element
+//   currentDiv.append(newLi);
+// }
 
-//populates list with array
-tasks.forEach(populateList);
+// //populates list with array
+// tasks.forEach(populateList);
 
 
 
@@ -136,12 +137,12 @@ let deleteItem = () => {
 
 
 /* Gets Data from FORM */
-let getData = () => {
-  let newTask = data.value;
-  console.log("Data Value: " + newTask);
-  // populateList();
-  return newTask;
-}
+// let getData = () => {
+//   let newTask = data.value;
+//   console.log("Data Value: " + newTask);
+//   // populateList();
+//   return newTask;
+// }
 
 /* Calls on eventListener for click event */
 form.addEventListener("click", addTask)
