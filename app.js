c6b59ec88f00ab4b8f615a6e7c45ffcc43d8b959
Jason <jimanproductions@gmail.com>
2022-05-04 13:28:30 +0200

    able to add task

--- app.js
+++ app.js
@@ -26,21 +26,48 @@
 /*Selects various dom elements*/
 let form = document.querySelector("#addItem");
 let data = document.querySelector("#task");
-let testli = document.querySelector("#testli"); 
+let testli = document.querySelector("#testli");
 let removes = document.querySelectorAll(".delete");
 
 let tasks = ["Walk Cat", "Walk Dog", "Walk Emu", "Talk to Emu", "Drink some beers", "Lift some weights or something"]
 let tasksFinished = [];
 
 /* Adds task to array function */
-let addTask = () =>{
-    tasks.push(getData());
-    console.log(tasks);
+let addTask = () => {
+  let newTask = getData();
+  tasks.push(newTask);
+  console.log(tasks);
+
+  const newLi = document.createElement("li");
+  const newLiContent = document.createTextNode(newTask);
+  const checkBox = document.createElement("input");
+  //creating new button
+  const button = document.createElement("button");
+  const deleteButton = document.createElement("button");
+
+  //sets element attributes
+  checkBox.type = "checkBox";
+  button.innerText = "Editbra!";
+  button.className = "edit";
+  deleteButton.innerText = "Delete";
+  deleteButton.className = "delete";
+
+  //append or add items to preexisting element
+  newLi.appendChild(checkBox);
+  newLi.appendChild(newLiContent);
+  newLi.appendChild(button);
+  newLi.appendChild(deleteButton);
+
+  //selects element you are adding to
+  const currentDiv = document.getElementById("ulthingie");
+  //adds element
+  currentDiv.append(newLi);
+
 }
 
 
 //Creates elements from array
-let populateList = (t) =>{
+let populateList = (t) => {
   const newLi = document.createElement("li");
   const newLiContent = document.createTextNode(t);
   const checkBox = document.createElement("input");
@@ -60,7 +87,7 @@ let populateList = (t) =>{
   newLi.appendChild(newLiContent);
   newLi.appendChild(button);
   newLi.appendChild(deleteButton);
-  
+
   //selects element you are adding to
   const currentDiv = document.getElementById("ulthingie");
   //adds element
@@ -101,7 +128,7 @@ let deleteItem = () => {
   console.log(this.parentNode);
   //when the button is clicked, then the element is removed from the screen...and eventually the array
   let listItem = this.parentNode;
-  let  ul = listItem.parentNode;
+  let ul = listItem.parentNode;
   ul.removeChild(listItem);
 }
 
@@ -110,18 +137,18 @@ let deleteItem = () => {
 
 /* Gets Data from FORM */
 let getData = () => {
-    let newTask = data.value;
-    console.log("Data Value: " + newTask);
-    // populateList();
-    return newTask;
+  let newTask = data.value;
+  console.log("Data Value: " + newTask);
+  // populateList();
+  return newTask;
 }
 
 /* Calls on eventListener for click event */
 form.addEventListener("click", addTask)
 
 /*Needs to put the remove in a loop as there are multiple objects that are not loaded based on the DOM or something like that.*/
-for (const remove of removes){
-remove.addEventListener("click", deleteItem)
+for (const remove of removes) {
+  remove.addEventListener("click", deleteItem)
 }
 
 
