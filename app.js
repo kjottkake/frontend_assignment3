3a63bd592b3f471194a99299bb92cebd9a08b687
Jason <jimanproductions@gmail.com>
2022-05-07 21:19:26 +0200

    delete works for all elements

--- app.js
+++ app.js
@@ -45,6 +45,8 @@ let testli = document.querySelector("#testli");
 
 //testing selecting lis
 let lists = document.querySelectorAll("li");
+
+
 let editforms = document.querySelectorAll("input.editForm");
 let enters = document.querySelectorAll("button.enter");
 let cancels = document.querySelectorAll("button.cancel");
@@ -53,7 +55,6 @@ let tasks = ["Walk Cat", "Walk Dog", "Walk Emu", "Talk to Emu", "Drink some beer
 let tasksFinished = [];
 
 let listCount = 5;
-
 
 
 /* Adds task to array function */
@@ -85,7 +86,7 @@ let addTask = () => {
 
   editForm.className = "editForm hidden";
   editConfirm.className = "enter hidden";
-  editCancel.className = "cancel hidden"; 
+  editCancel.className = "cancel hidden";
 
 
   //append or add items to preexisting element
@@ -109,45 +110,24 @@ let addTask = () => {
   data.value = '';
   // getRemoves();
   listCounter();
+  deleteCounter();
 }
 
 //tests count for redefinition of selected dom stuff
-let listCounter = () =>{
+let listCounter = () => {
   let listCount = document.querySelectorAll("li");
   return listCount;
 }
 
-//Creates elements from array
-// let populateList = (t) => {
-//   const newLi = document.createElement("li");
-//   const newLiContent = document.createTextNode(t);
-//   const checkBox = document.createElement("input");
-//   //creating new button
-//   const button = document.createElement("button");
-//   const deleteButton = document.createElement("button");
-
-//   //sets element attributes
-//   checkBox.type = "checkBox";
-//   button.innerText = "Editbra!";
-//   button.className = "edit";
-//   deleteButton.innerText = "Delete";
-//   deleteButton.className = "delete";
-
-//   //append or add items to preexisting element
-//   newLi.appendChild(checkBox);
-//   newLi.appendChild(newLiContent);
-//   newLi.appendChild(button);
-//   newLi.appendChild(deleteButton);
-
-//   //selects element you are adding to
-//   const currentDiv = document.getElementById("ulthingie");
-//   //adds element
-//   currentDiv.append(newLi);
-// }
-
-// //populates list with array
-// tasks.forEach(populateList);
-
+let deleteCounter = () => {
+  let deleteCount = document.querySelectorAll("button.delete");
+  for (let i = 0; i < listCount; i++) {
+    deleteCount[i].addEventListener("click", function (e) {
+      deleteItem(e);
+    });
+  }
+  return deleteCount;
+}
 
 /*Delete item */
 let deleteItem = (e) => {
@@ -155,26 +135,25 @@ let deleteItem = (e) => {
   console.log("e.target: " + e.target); //where is it being clicked?
   //when the button is clicked, then the element is removed from the screen...and eventually the array
   let target = e.target;
-  listItem  = target.parentNode;
+  listItem = target.parentNode;
   listItem.remove();
   listCount = listCount - 1;
   return 0;
 }
 
-
-let getFormValue = (e) =>{
+let getFormValue = (e) => {
   //specifiy form number
   let listItem = e.target.parentNode;
   let text = listItem.querySelector('p');
   //get the value
-  return text; 
+  return text;
   //display value
 
   //return value
 }
 
 /*Edit Item*/
-let editItem = (i, e) =>{
+let editItem = (i, e) => {
   console.log("You clicked on the edit button!");
   editforms[i].classList.remove('hidden');
   enters[i].classList.remove('hidden');
@@ -201,31 +180,22 @@ let editItem = (i, e) =>{
 /* Calls on eventListener for click event */
 form.addEventListener("click", addTask)
 
-/*Needs to put the remove in a loop as there are multiple objects that are not loaded based on the DOM or something like that.*/
-// for (const remove of removes) {
-//   remove.addEventListener("click", deleteItem(e))
-// }
-
 let removes = document.querySelectorAll("button.delete");
-let edits = document.querySelectorAll("button.edit"); 
+let edits = document.querySelectorAll("button.edit");
 
-let getRemoves = () =>{
-  let removes = document.querySelectorAll("button.delete");
-  return removes;
-}
 
-for (let i = 0; i < listCount; i++){
-  // getRemoves();
-// let removes = document.querySelectorAll("button.delete");
-removes[i].addEventListener("click", function (e){
-  deleteItem(e);
-});
 
-// edit.addEventListener("click", editItem)
-edits[i].addEventListener("click", function (e){
-  getFormValue(e);
-  editItem(i, e);
-});
+
+for (let i = 0; i < listCount; i++) {
+  removes[i].addEventListener("click", function (e) {
+    deleteItem(e);
+  });
+
+  // edit.addEventListener("click", editItem)
+  edits[i].addEventListener("click", function (e) {
+    getFormValue(e);
+    editItem(i, e);
+  });
 }
 
 
--- test.js
+++ test.js
@@ -1,12 +1,32 @@
-//selects forms element from html
-let editforms = document.querySelectorAll("input.editForm");
 
-let addFunction = () =>{
-    //this function creates a new html element
-}
+//Creates elements from array
+// let populateList = (t) => {
+//   const newLi = document.createElement("li");
+//   const newLiContent = document.createTextNode(t);
+//   const checkBox = document.createElement("input");
+//   //creating new button
+//   const button = document.createElement("button");
+//   const deleteButton = document.createElement("button");
 
-let functionToRunWhenUserClicksOnNewHTMLElement = () =>{
-    //this function should run when the user clicks on the new element.
-}
+//   //sets element attributes
+//   checkBox.type = "checkBox";
+//   button.innerText = "Editbra!";
+//   button.className = "edit";
+//   deleteButton.innerText = "Delete";
+//   deleteButton.className = "delete";
 
-//Question: how I update the original number of forms selects, or how do I select the newly created html element?
+//   //append or add items to preexisting element
+//   newLi.appendChild(checkBox);
+//   newLi.appendChild(newLiContent);
+//   newLi.appendChild(button);
+//   newLi.appendChild(deleteButton);
+
+//   //selects element you are adding to
+//   const currentDiv = document.getElementById("ulthingie");
+//   //adds element
+//   currentDiv.append(newLi);
+// }
+
+// //populates list with array
+// tasks.forEach(populateList);
+
